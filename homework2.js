43294a40fd6fa28555aa0913445abbb73a7f40b0
ashkhenvoskanyan <voskanyanashxen@gmail.com>
2018-09-08 15:04:01 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,17 @@
+const a = 5;
+const b = 7;
+const c = 9;
+alert (a + b );
+alert (b * c );
+alert (c - a );
+
+const val1 = 5
+const val2 = "hi"
+const val3 = 9
+
+if (val1 === val2 || val1 !== val3)
+{console.log ("good job"); }
+else { console.log ("bad")}
+if(a + b + c !== 45) {
+	console.log("hiiiiii!!!!");
+}
